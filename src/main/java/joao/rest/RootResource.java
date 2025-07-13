80b9a02b43090e49e23f312f3480e08c119885fd
Joao Emanoel Rodrigues Mota <jooemanoel@gmail.com>
2025-07-12 23:33:37 -0300

    teste

--- src/main/java/joao/rest/RootResource.java
+++ src/main/java/joao/rest/RootResource.java
@@ -11,19 +11,24 @@ public class RootResource {
     @GET
     @Produces(MediaType.TEXT_HTML)
     public String hello() {
-        return "<!DOCTYPE html><html lang=\"en\">\n" +
-               "<head>\n" +
-               "    <meta charset=\"UTF-8\">\n" +
-               "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
-               "    <title>Greeting</title>\n" +
-               "</head>\n" +
-               "<body style=\"text-align:center;\">\n" +
-               "    <h1>A aplicação está funcionando!</h1>\n" +
-               "    <h1>Principais endpoints:</h2>\n" +
-               "    <h3>/usuarios</h3>\n" +
-               "    <h3>/listas</h3>\n" +
-               "    <h3>/itens</h3>\n" +
-               "</body>\n" +
-               "</html>";
+        return "<!DOCTYPE html>\r\n" + //
+                        "<html lang=\"en\">\r\n" + //
+                        "  <head>\r\n" + //
+                        "    <meta charset=\"UTF-8\" />\r\n" + //
+                        "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />\r\n" + //
+                        "    <title>Listas - API</title>\r\n" + //
+                        "  </head>\r\n" + //
+                        "  <body style=\"text-align: center; background-color: darkgray; color: #333\">\r\n" + //
+                        "    <h1>Listas - API</h1>\r\n" + //
+                        "    <p>Bem-vindo à documentação da API de Listas.</p>\r\n" + //
+                        "    <p>\r\n" + //
+                        "      Esta API permite que você gerencie listas de itens de forma eficiente.\r\n" + //
+                        "    </p>\r\n" + //
+                        "    <p>Você pode criar, ler, atualizar e excluir listas e seus itens.</p>\r\n" + //
+                        "    <br />\r\n" + //
+                        "    <a href=\"/swagger-ui\">Swagger</a>\r\n" + //
+                        "  </body>\r\n" + //
+                        "</html>\r\n" + //
+                        "";
     }
 }
